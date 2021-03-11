548522cd141401e9e41a3221f697f969929baa5d
Ratchathorn123 <79511320+Ratchathorn123@users.noreply.github.com>
2021-03-11 14:55:21 +0700

    Add files via upload

---
+++ Homework_3_Week5.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Homework_3_Week5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String ModeProgram = Console.ReadLine();
+            int x = Console.ReadLine();
+            int y = Console.ReadLine();
+
+            if (ModeProgram = "price")
+            {
+                Console.WriteLine();
+            }
+            if (ModeProgram = "time")
+            {
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Invalid mode");
+            }
+            if (x = 1)
+            {
+                Console.WriteLine();
+            }
+            else if(y = 2.5)
+            {
+                Console.WriteLine("Invalid mouse position");
+            }
+            Console.ReadLine();
+        }
+    }
+}
